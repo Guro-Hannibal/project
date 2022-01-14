c767a3b785d18e8f1011cdf8b2d22b4893b2a8fd
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-14 14:30:07 +0300

    Update core.py

--- Source/core.py
+++ Source/core.py
@@ -1,11 +1,15 @@
-from mysql.connector import connect, Error
-import sshtunnel
-import go
+
 global tunnel
+
+import sshtunnel
+global tunnel
+from peewee import *
+import datetime
+import foodb
 
 tunnel = sshtunnel.SSHTunnelForwarder(
     ssh_username='ag3131',
-    ssh_password=go.codemore,
+    ssh_password='GoForthGo',
     ssh_address_or_host='192.168.0.66',
     ssh_port=22,
     ssh_pkey='',
@@ -15,21 +19,150 @@ tunnel = sshtunnel.SSHTunnelForwarder(
 
 tunnel.start()
 
-global connection
+class Puller(Database):
+    def _connect(self):
+        return foodb.(connect)
 
-try:
 
-    connection = connect (
-        host='localhost',
-        user='root',
-        passwd='Science',
-        port=tunnel.local_bind_port
-    )
 
-except Error:
-    print(f'Ошибка -  {Error}')
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-cursor = connection.cursor()
+
+
+class User(BaseModel):
+    username = CharField(unique=True)
+
+class Tweet(BaseModel):
+    user = ForeignKeyField(User, backref='tweets')
+    message = TextField()
+    created_date = DateTimeField(default=datetime.datetime.now())
+    is_published = BooleanField(default=True)
+
+
+db.connect()
+
+db.execute('USE information_schema')
+
+
+
+# from mysql.connector import connect, Error
+# import sshtunnel
+# import go
+# global tunnel
+#
+# tunnel = sshtunnel.SSHTunnelForwarder(
+#     ssh_username='ag3131',
+#     ssh_password=go.codemore,
+#     ssh_address_or_host='192.168.0.66',
+#     ssh_port=22,
+#     ssh_pkey='',
+#     remote_bind_address=('127.0.0.1', 3306)
+# )
+#
+#
+# tunnel.start()
+#
+# global connection
+#
+# try:
+#
+#     connection = connect (
+#         host='localhost',
+#         user='root',
+#         passwd='Science',
+#         port=tunnel.local_bind_port
+#     )
+#
+# except Error:
+#     print(f'Ошибка -  {Error}')
+#
+# cursor = connection.cursor()
+# #
+# # cursor.execute('SELECT CURDATE()')
+# #
+# # row = cur.fetchone()
+# #
+# # print(f"Date - {row}")
+#
+# # cur.execute('DESCRIBE events;')
+# # row = cur.fetchall()
+# # for el in row:
+# #     print(el)
+# #
+# # cursor.execute('USE information_schema;')
+# # ox = 'location_history'
+# # db = 'information_schema'
+# # fdsfsdf = 'schema_name'
+# # cur.execute("""SELECT * FROM tables WHERE (ab LIKE %s{fgffname}% and lcname LIKE%{ffaf}%)""")%(ox,fdsfsdf)
+# # row = cur.fetchall()
+# #
+# # print(row)
+#
+# dbcfname = 'table_name'
+#
+# dbclname = 'schema_name'
+#
+# dbcaddress = 'location_history'
+#
+# dbccity = 'users'
+#
+# dbcstate = 'vars'
+#
+# dbczipcode = 'muhaha'
+#
+# cursor.execute('SELECT * FROM information_schema.tables')
+#
+# bcname = cursor.fetchall()
+
+# cursor.execute(f"SELECT UNION(''s'', ''ys'') FROM '{(bcname, )}")
+#
+# bcname = cursor.fetchall()
+#
+# print(bcname)
+
+# for el in bcname:
+#     print(el)
+#
+# connection.close()
+
+tunnel.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# global connection
+#
+# try:
+#
+#     connection = connect (
+#         host='localhost',
+#         user='root',
+#         passwd='Science',
+#         port=tunnel.local_bind_port
+#     )
+#
+# except Error:
+#     print(f'Ошибка -  {Error}')
+#
+# cursor = connection.cursor()
 #
 # cursor.execute('SELECT CURDATE()')
 #
@@ -51,21 +184,21 @@ cursor = connection.cursor()
 #
 # print(row)
 
-dbcfname = 'table_name'
-
-dbclname = 'schema_name'
-
-dbcaddress = 'location_history'
-
-dbccity = 'users'
-
-dbcstate = 'vars'
-
-dbczipcode = 'muhaha'
-
-cursor.execute('SELECT * FROM information_schema.tables')
-
-bcname = cursor.fetchall()
+# dbcfname = 'table_name'
+#
+# dbclname = 'schema_name'
+#
+# dbcaddress = 'location_history'
+#
+# dbccity = 'users'
+#
+# dbcstate = 'vars'
+#
+# dbczipcode = 'muhaha'
+#
+# cursor.execute('SELECT * FROM information_schema.tables')
+#
+# bcname = cursor.fetchall()
 
 # cursor.execute(f"SELECT UNION(''s'', ''ys'') FROM '{(bcname, )}")
 #
@@ -73,9 +206,9 @@ bcname = cursor.fetchall()
 #
 # print(bcname)
 
-for el in bcname:
-    print(el)
-
-connection.close()
+# for el in bcname:
+#     print(el)
+#
+# connection.close()
 
 tunnel.close()
