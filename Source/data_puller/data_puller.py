1ed5af54d5ca07a62f1cf02487df1579e74580e5
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-16 10:59:05 +0300

    Update data_puller.py

--- Source/data_puller/data_puller.py
+++ Source/data_puller/data_puller.py
@@ -9,3 +9,14 @@ def get_rows(table_name, cursor, count):
     tuple_result = cursor.fetchall()
     print(tuple_result)
     return tuple_result
+
+
+def get_custom_rows(cursor, table_name, name, *args):
+    rows = []
+    for el in args:
+        exe_str = (
+            f"SELECT * FROM {table_name} WHERE {name}='{el}'"
+                   )
+        cursor.execute(exe_str,)
+        rows.append(cursor.fetchone())
+    return rows
