2aaf3dbc096d6f2e0207acc25866ce8db916614e
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-16 10:33:15 +0300

    Update core.py

--- Source/core.py
+++ Source/core.py
@@ -1,5 +1,5 @@
 from realation_connector.some_connector import tunnel, sql_connect, sql_connect_error_catcher
-from data_puller.data_puller import get_rows, get_tables_count
+from data_puller.data_puller import get_rows, get_rows_count
 from data_distributor.data_distributor import distribute
 from data_pusher.data_pusher import insert_into_table, insert_many_tables
 
@@ -11,7 +11,7 @@ cur.execute('SELECT CURDATE()')
 
 row = cur.fetchone()
 
-# print(f"Date - {row}")
+print(f"Date - {row}")
 
 cur.execute('DESCRIBE sys;')
 row = cur.fetchall()
@@ -19,13 +19,13 @@ for el in row:
     print(el)
 
 
-get_tables_count('sys', cur)
+get_rows_count('sys', cur)
 
 
-# print(get_tables_count('sys', cur))
+print(get_rows_count('sys', cur))
 
 
-tables_rows = get_rows('sys', cur, get_tables_count('sys', cur))
+tables_rows = get_rows('sys', cur, get_rows_count('sys', cur))
 
 platforms_table = []
 customers_table = []
@@ -33,33 +33,37 @@ suppliers_table = []
 services_table = []
 locations_table = []
 
-print('333333333333333')
-
-
 distribute(tables_rows, platforms_table, customers_table, suppliers_table, services_table, locations_table)
 
-print(platforms_table)
-print(customers_table)
-print(suppliers_table)
-print(services_table)
-print(locations_table)
+# print(platforms_table)
+# print(customers_table)
+# print(suppliers_table)
+# print(services_table)
+# print(locations_table)
 
 cur.execute('USE friendship')
 
 # insert_into_table('platforms', cur, platforms_table)
-connection.commit()
-# str_data = (
-#         "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
-#     )
-# cur.executemany(str_data, platforms_table)
-insert_many_tables(cur, 5, platforms_table, customers_table, suppliers_table, services_table, locations_table, 'platforms', 'customers', 'suppliers', 'services', 'locations')
+# connection.commit()
+
+insert_many_tables(cur, 5, platforms_table, customers_table, suppliers_table, services_table, locations_table,
+                   'platforms', 'customers', 'suppliers', 'services', 'locations')
 connection.commit()
 
-print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
-
+tables = []
 cur.execute('SELECT * FROM platforms')
-print(cur.fetchall())
-connection.close()
+tables.append(cur.fetchall())
+cur.execute('SELECT * FROM customers')
+tables.append(cur.fetchall())
+cur.execute('SELECT * FROM suppliers')
+tables.append(cur.fetchall())
+cur.execute('SELECT * FROM services')
+tables.append(cur.fetchall())
+cur.execute('SELECT * FROM locations')
+tables.append(cur.fetchall())
+for el in tables:
+    print(el)
 
+connection.close()
 tunnel.close()
 
