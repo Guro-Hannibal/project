b19cd8284c7772be01a1f9b96e069c9b45ed4a10
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-16 09:31:57 +0300

    Update data_pusher.py

--- Source/data_pusher/data_pusher.py
+++ Source/data_pusher/data_pusher.py
@@ -1,11 +1,27 @@
+def insert_into_table(data, cursor, table_name):
+    if table_name == 'platfroms':
+        str_data = (
+            "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        )
+    elif table_name == 'suppliers':
+        str_data = (
+            "INSERT INTO suppliers (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        )
+    elif table_name == 'services':
+            str_data = (
+                "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+            )
+    elif table_name == 'locations':
+        str_data = (
+            "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        )
+    elif table_name == 'customers':
+        str_data = (
+            "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        )
+    else:
+        return print('Not enough data: table_name doesnt much')
+    cursor.executemany(str_data, data)
 
-def insert_into_table(table_name, data, cursor):
-    for row in data:
-        i = 0
-        exe_string = f'INSERT INTO {table_name} (*) VALUES'
-        for el in row:
-            if i < len(row):
-                exe_string += f' {el},'
-            else:
-                break
-        cursor.execute(f'INSERT INTO {table_name} (*) VALUES')
+
+def insert_many_tables(cursor, *args):
