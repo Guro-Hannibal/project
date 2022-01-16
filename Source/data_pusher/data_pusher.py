142ddc3a4001260489b07ae5daaa0b43e4b21e56
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-16 10:24:22 +0300

    Update data_pusher.py

--- Source/data_pusher/data_pusher.py
+++ Source/data_pusher/data_pusher.py
@@ -1,27 +1,33 @@
-def insert_into_table(data, cursor, table_name):
-    if table_name == 'platfroms':
-        str_data = (
+def insert_into_table(table_name, cursor, data):
+    if table_name == 'platforms':
+        sql_str = (
             "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
         )
     elif table_name == 'suppliers':
-        str_data = (
-            "INSERT INTO suppliers (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        sql_str = (
+            "INSERT INTO suppliers (supplier_id, supplier_name, supplier_details) VALUES(%s, %s, %s)"
         )
     elif table_name == 'services':
-            str_data = (
-                "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        sql_str = (
+                "INSERT INTO services (service_code, service_name, service_details) VALUES(%s, %s, %s)"
             )
     elif table_name == 'locations':
-        str_data = (
-            "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        sql_str = (
+            "INSERT INTO locations (location_id, location_name, location_details) VALUES(%s, %s, %s)"
         )
     elif table_name == 'customers':
-        str_data = (
-            "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
+        sql_str = (
+            "INSERT INTO customers (customer_name, customer_id, title, gender, customers_details) VALUES(%s, %s, %s, %s, %s)"
         )
     else:
         return print('Not enough data: table_name doesnt much')
-    cursor.executemany(str_data, data)
+    cursor.executemany(sql_str, data)
 
 
-def insert_many_tables(cursor, *args):
+def insert_many_tables(cursor, count, *args):
+    print('fdsssssssssssssssssssssssssssssssssssssssssssssssssssssssss')
+    print(args)
+    for el in args[:count]:
+        print(args[count])
+        insert_into_table(args[count], cursor, el)
+        count+=1
