91c294ba86634ba5a79f68a10831868010a2e9db
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-16 12:49:51 +0300

    Update data_puller.py

--- Source/data_puller/data_puller.py
+++ Source/data_puller/data_puller.py
@@ -7,16 +7,27 @@ def get_rows_count(table_name, cursor):
 def get_rows(table_name, cursor, count):
     cursor.execute(f'SELECT * FROM {table_name}')
     tuple_result = cursor.fetchall()
-    print(tuple_result)
     return tuple_result
 
 
-def get_custom_rows(cursor, table_name, name, *args):
-    rows = []
+def get_table_names(cursor):
+    cursor.execute('SHOW TABLES')
+    return cursor.fetchall()
+
+
+def get_custom_rows(cursor, table_name, condition, *args):
+    __count = get_rows_count(table_name, cursor)
+    result_rows = []
     for el in args:
-        exe_str = (
-            f"SELECT * FROM {table_name} WHERE {name}='{el}'"
-                   )
-        cursor.execute(exe_str,)
-        rows.append(cursor.fetchone())
-    return rows
+        sql_str = (
+        f"SELECT * FROM {table_name} WHERE {condition}='{el}'"
+                    )
+        cursor.execute(sql_str,)
+        result_rows.append(cursor.fetchall())
+    return result_rows
+
+# tables_names and conditions must be dictionary and iterable,
+# conditions must be iterable if more than 1 for a table put in another iterable argument
+# for example tuples in list
+
+
