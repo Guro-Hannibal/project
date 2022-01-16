0773aa236eb1eb4df76aca822376668be1a97c90
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-17 02:16:04 +0300

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,6 @@
+INSERT INTO suppliers (supplier_id, supplier_name, supplier_details) VALUES (423423, 'dfg', 'fsfsd');
+INSERT INTO suppliers (supplier_id, supplier_name, supplier_details) VALUES (978978, 'ouio', 'fsasdd');
+INSERT INTO services (service_code, service_name, supplier_name) VALUES (12314, 'abc', 'dfg');
+INSERT INTO services (service_code, service_name, supplier_name) VALUES (132123, 'oiuoui', 'ouio');
+
+SELECT * FROM suppliers_services_join;
