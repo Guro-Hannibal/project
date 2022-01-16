6e1d72a9e60d66ffbf74912eadcce925d11d739a
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-16 12:49:43 +0300

    Update core.py

--- Source/core.py
+++ Source/core.py
@@ -1,5 +1,5 @@
 from realation_connector.some_connector import tunnel, sql_connect, sql_connect_error_catcher
-from data_puller.data_puller import get_rows, get_rows_count, get_custom_rows
+from data_puller.data_puller import get_rows, get_rows_count, get_custom_rows, get_table_names
 from data_distributor.data_distributor import distribute
 from data_pusher.data_pusher import insert_into_table, insert_many_tables
 
@@ -13,13 +13,23 @@ row = cur.fetchone()
 
 print(f"Date - {row}")
 
+row = get_table_names(cur)
+print(row)
+
+
+def foo():
+    print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
+
+
+foo()
+
+
 cur.execute('DESCRIBE sys;')
 row = cur.fetchall()
 for el in row:
     print(el)
 
-
-get_rows_count('sys', cur)
+foo()
 
 
 print(get_rows_count('sys', cur))
@@ -35,20 +45,28 @@ locations_table = []
 
 distribute(tables_rows, platforms_table, customers_table, suppliers_table, services_table, locations_table)
 
-# print(platforms_table)
-# print(customers_table)
-# print(suppliers_table)
-# print(services_table)
-# print(locations_table)
+print(platforms_table)
+print(customers_table)
+print(suppliers_table)
+print(services_table)
+print(locations_table)
+
+
+foo()
+
 
 cur.execute('USE friendship')
 
-# insert_into_table('platforms', cur, platforms_table)
-# connection.commit()
+insert_into_table('platforms', cur, platforms_table)
+connection.commit()
 
-# insert_many_tables(cur, 5, platforms_table, customers_table, suppliers_table, services_table, locations_table,
-#                    'platforms', 'customers', 'suppliers', 'services', 'locations')
-# connection.commit()
+insert_many_tables(cur, 4, customers_table, suppliers_table, services_table, locations_table,
+                   'customers', 'suppliers', 'services', 'locations')
+connection.commit()
+
+
+foo()
+
 
 tables = []
 cur.execute('SELECT * FROM platforms')
@@ -64,6 +82,7 @@ tables.append(cur.fetchall())
 for el in tables:
     print(el)
 
+foo()
 
 custom_rows = get_custom_rows(cur, 'suppliers', 'supplier_name', 'fsdfds', 'fsdfsf')
 print(custom_rows)
