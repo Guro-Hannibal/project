0eb3aecfc00c55afce8fcdbba8ece3e9a0ce3e00
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-16 10:24:18 +0300

    Update core.py

--- Source/core.py
+++ Source/core.py
@@ -1,7 +1,7 @@
 from realation_connector.some_connector import tunnel, sql_connect, sql_connect_error_catcher
 from data_puller.data_puller import get_rows, get_tables_count
 from data_distributor.data_distributor import distribute
-from data_pusher.data_pusher import insert_into_table
+from data_pusher.data_pusher import insert_into_table, insert_many_tables
 
 tunnel.start()
 
@@ -39,21 +39,21 @@ print('333333333333333')
 distribute(tables_rows, platforms_table, customers_table, suppliers_table, services_table, locations_table)
 
 print(platforms_table)
-# print(customers_table)
-# print(suppliers_table)
-# print(services_table)
-# print(locations_table)
+print(customers_table)
+print(suppliers_table)
+print(services_table)
+print(locations_table)
 
 cur.execute('USE friendship')
 
-insert_into_table('platforms', platforms_table, cur)
+# insert_into_table('platforms', cur, platforms_table)
 connection.commit()
 # str_data = (
 #         "INSERT INTO platforms (platform_id, platform_name, platform_details, acces_variables) VALUES(%s, %s, %s, %s)"
 #     )
 # cur.executemany(str_data, platforms_table)
-
-
+insert_many_tables(cur, 5, platforms_table, customers_table, suppliers_table, services_table, locations_table, 'platforms', 'customers', 'suppliers', 'services', 'locations')
+connection.commit()
 
 print('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
 
