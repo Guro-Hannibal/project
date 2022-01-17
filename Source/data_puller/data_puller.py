9f11826e5af429a7431bfed3886a991b29b00d80
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-17 12:39:42 +0300

    Update data_puller.py

--- Source/data_puller/data_puller.py
+++ Source/data_puller/data_puller.py
@@ -26,6 +26,21 @@ def get_custom_rows(cursor, table_name, condition, *args):
         result_rows.append(cursor.fetchall())
     return result_rows
 
+
+def join_custom_rows(cursor, join_type, join_table, joiner_table, condition, second_condition, list_of_args):
+    sql_str = f"SELECT "
+    for num, el in enumerate(list_of_args):
+        if num >= len(list_of_args) - 1:
+            sql_str += f"{join_table}.{el} "
+            break
+        sql_str += f"{join_table}.{el}, "
+    result_sql_str = (
+        sql_str +
+        f"FROM {join_table} "
+        f"{join_type} {joiner_table} ON {join_table}.{condition} = {joiner_table}.{second_condition};"
+    )
+    return cursor.execute(result_sql_str)
+
 # tables_names and conditions must be dictionary and iterable,
 # conditions must be iterable if more than 1 for a table put in another iterable argument
 # for example tuples in list
