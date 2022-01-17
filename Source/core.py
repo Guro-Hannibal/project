d65705e87b19dc2ddf2591407c6669d6745b05b5
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-17 22:23:30 +0300

    Update core.py

--- Source/core.py
+++ Source/core.py
@@ -1,11 +1,33 @@
-from relation_connector.relation_connector import tunnel, sql_connect, sql_connect_error_catcher
-from data_puller.data_puller import get_rows, get_rows_count, get_custom_rows, get_table_names, join_custom_rows
-from data_distributor.data_distributor import distribute
-from data_pusher.data_pusher import insert_into_table, insert_many_tables, trigger_check
 
-tunnel.start()
+# from relation_connector.relation_connector import tunnel, sql_connect, sql_connect_error_catcher
+# from data_puller.data_puller import get_rows, get_rows_count, get_custom_rows, get_table_names, join_custom_rows, \
+#     union_custom_rows
+# from data_distributor.data_distributor import distribute
+# from data_pusher.data_pusher import insert_into_table, insert_many_tables, trigger_check, \
+#     supplier_trigger, service_trigger
 
-connection, cur = sql_connect_error_catcher(sql_connect())
+
+class Core:
+
+    def __init__(self, database_source, database_friendship):
+        self.database_source = database_source
+        self.database_friendship = database_friendship
+        self.connection, self.interface = sql_connect_error_catcher(sql_connect())
+        self.__pusher = Source.DataPusher
+
+
+    def __import_puller(self):
+        pass
+
+    def __import_distribotor(self):
+        pass
+
+
+
+core = Core('sys', 'friendship',)
+
+
+
 
 cur.execute('SELECT CURDATE()')
 
@@ -57,6 +79,7 @@ foo()
 
 cur.execute('USE friendship')
 
+
 insert_into_table('platforms', cur, platforms_table)
 connection.commit()
 
@@ -67,7 +90,8 @@ connection.commit()
 
 foo()
 
-trigger_check(cur)
+if supplier_trigger and service_trigger:
+    trigger_check(cur)
 
 
 tables = []
@@ -90,10 +114,15 @@ custom_rows = get_custom_rows(cur, 'suppliers', 'supplier_name', 'fsdfds', 'fsdf
 print(custom_rows)
 
 
-join_custom_rows(cur, 'LEFT JOIN', 'suppliers', 'services', 'supplier_name', 'supplier_name',
-                       ['supplier_id', 'supplier_name', 'supplier_details'])
+# join_custom_rows(cur, 'LEFT JOIN', 'suppliers', 'services', 'supplier_name', 'supplier_name',
+#                        ['supplier_id', 'supplier_name', 'supplier_details'])
+
+
+union_custom_rows(cur, "suppliers", "services", ['supplier_id', 'supplier_name', 'supplier_details'],
+                  ['service_code', 'service_name', 'services_details'], [], False)
 
 print(cur.fetchall())
+
 
 connection.close()
 tunnel.close()
