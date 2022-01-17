2aee69c5da3751198569efb339853e0d41ce8d27
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-17 22:23:36 +0300

    Update data_puller.py

--- Source/data_puller/data_puller.py
+++ Source/data_puller/data_puller.py
@@ -39,8 +39,50 @@ def join_custom_rows(cursor, join_type, join_table, joiner_table, condition, sec
         f"FROM {join_table} "
         f"{join_type} {joiner_table} ON {join_table}.{condition} = {joiner_table}.{second_condition};"
     )
+    print()
     return cursor.execute(result_sql_str)
 
+
+def union_custom_rows(cursor, union_table, unioner_table, args_list, atr_list, conditions_list, order = False):
+    sql_str_top = f"SELECT "
+    sql_str_bottom = f"SELECT "
+    sql_str_universal = ''
+    str_sql_obedient = ''
+    for num, el in enumerate(args_list):
+        if num >= len(args_list) - 1:
+            sql_str_top += f"{union_table}.{el}"
+            break
+        sql_str_top += f"{union_table}.{el}, "
+    for num, atr in enumerate(atr_list):
+        if num >= len(atr_list) - 1:
+            sql_str_bottom += f"{unioner_table}.{atr} FROM {unioner_table}"
+            break
+        sql_str_bottom += f"{unioner_table}.{atr}, "
+
+    if len(conditions_list) == 0:
+        sql_str_universal = f" FROM {union_table} "
+
+    elif len(conditions_list) <= 2:
+        sql_str_universal = f" FROM {union_table} ON {union_table}.{conditions_list[0]} " \
+                         f"= {unioner_table}.{conditions_list[1]} " \
+                            f""
+
+    elif len(conditions_list) > 2:
+        sql_str_universal = f" FROM {union_table} ON {union_table}.{conditions_list[0]} " \
+                         f"= {union_table}.{conditions_list[0]}" \
+                        f"{union_table} ON {union_table}.{conditions_list[0]} = {unioner_table}.{conditions_list} "
+
+    if order:
+        str_sql_obedient = f" ORDER BY {order}"
+    master = (
+        sql_str_top +
+        sql_str_universal +
+        "UNION " +
+        sql_str_bottom +
+        str_sql_obedient + ';'
+    )
+    print(master)
+    cursor.execute(master)
 # tables_names and conditions must be dictionary and iterable,
 # conditions must be iterable if more than 1 for a table put in another iterable argument
 # for example tuples in list
