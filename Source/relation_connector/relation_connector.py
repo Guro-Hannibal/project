42104688af25d012e1a7d4d8167ac547a48ef61e
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-17 13:26:42 +0300

    Create relation_connector.py

---
+++ Source/relation_connector/relation_connector.py
@@ -0,0 +1,30 @@
+from mysql.connector import connect, Error
+import sshtunnel
+
+tunnel = sshtunnel.SSHTunnelForwarder(
+    ssh_username='ag3131',
+    ssh_password='GoForthGo',
+    ssh_address_or_host='192.168.0.66',
+    ssh_port=22,
+    remote_bind_address=('127.0.0.1', 3306)
+)
+
+
+def sql_connect():
+    connection = connect(
+        host='localhost',
+        user='root',
+        passwd='Science',
+        database='data_source',
+        port=tunnel.local_bind_port)
+    cur = connection.cursor()
+    return connection, cur
+
+
+def sql_connect_error_catcher(foo):
+    try:
+        relay, cur = foo
+        return relay, cur
+    except Error:
+        print(f'Ошибка -  {Error}')
+
