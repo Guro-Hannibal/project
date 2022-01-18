2c17e75f7fc6aba1708d10ffb8e47f8496c94ec2
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-18 19:21:34 +0300

    Create data_distributor.py

---
+++ Source/distributor/data_distributor.py
@@ -0,0 +1,19 @@
+
+class Distributor:
+
+    def distribute(self, rows, *args):
+        i = 0
+        for row in rows:
+            args[i].append(row[0:4])
+            i += 1
+            args[i].append(row[4:9])
+            i += 1
+            args[i].append(row[9:12])
+            i += 1
+            args[i].append(row[12:15])
+            i += 1
+            args[i].append(row[15:])
+            i += 1
+            i = 0
+        return args
+
