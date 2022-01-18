aba5e47d7792243f0d07930b16b42a4351204e4b
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-18 19:23:15 +0300

    Update relation_connector.py

--- Source/relation_connector/relation_connector.py
+++ Source/relation_connector/relation_connector.py
@@ -2,23 +2,21 @@ from mysql.connector import connect, Error
 import sshtunnel
 
 tunnel = sshtunnel.SSHTunnelForwarder(
-    ssh_username='ag3131',
-    ssh_password='GoForthGo',
-    ssh_address_or_host='192.168.0.66',
-    ssh_port=22,
-    remote_bind_address=('127.0.0.1', 3306)
-)
+        ssh_username='ag3131',
+        ssh_password='GoForthGo',
+        ssh_address_or_host='192.168.0.66',
+        ssh_port=22,
+        remote_bind_address=('127.0.0.1', 3306)
+    )
 
 
 def sql_connect():
-    connection = connect(
+        connection = connect(
         host='localhost',
         user='root',
         passwd='Science',
         database='data_source',
         port=tunnel.local_bind_port)
-    cur = connection.cursor()
-    return connection, cur
 
 
 def sql_connect_error_catcher(foo):
@@ -27,4 +25,6 @@ def sql_connect_error_catcher(foo):
         return relay, cur
     except Error:
         print(f'Ошибка -  {Error}')
+
+
 
