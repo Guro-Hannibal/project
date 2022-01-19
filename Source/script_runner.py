86d0d5bd1ee2d4584c2d1ff24cf52b4aafe7db83
Guro-Hannibal <92650449+Guro-Hannibal@users.noreply.github.com>
2022-01-19 04:36:57 +0300

    Update script_runner.py

--- Source/script_runner.py
+++ Source/script_runner.py
@@ -1,11 +1,9 @@
-import core
 import outer_gateway
 
 
-console = core.__UI__('source_database', 'friendship', core.relay, core.interface)
+console = core.UI('source_database', 'friendship', core.relay, core.interface)
 
-console
 
-outer_gateway.foo(console)
+outer_gateway.foo(console.interface())
 
 
